059bfb3b76e2bafd9845502f8fe772b41fead273
OskarUlrich <oskar.ulrich@email.cz>
2022-08-15 15:03:48 +0200

    vytvořil jsem soubor třídu Pojisteny.java a přidal jí atributy

---
+++ Pojisteny.java
@@ -0,0 +1,27 @@
+/**
+ * Reprezentuje pojištěného v databázi pojištených
+ */
+public class Pojisteny {
+
+    /**
+     * Jméno
+     */
+    private String jmeno; // nabízí final - co to je????????????????????????????
+
+    /**
+     * Příjmení
+     */
+    private String prijmeni;
+
+    /**
+     * Věk v letech
+     */
+    private int vek;
+
+    /**
+     * Telefonní číslo
+     */
+    private int telefonniCislo;
+
+
+}
