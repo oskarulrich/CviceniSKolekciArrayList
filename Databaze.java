10d8964700c5ab68f3d43080d9e100d8e10d652f
OskarUlrich <oskar.ulrich@email.cz>
2022-08-15 16:24:06 +0200

    v souboru Evidence.java jsem přidal parametry třídy a konstruktor (později doplním metody)

--- Databaze.java
+++ Databaze.java
@@ -1,2 +1,40 @@
-public class Databaze {
+import java.util.ArrayList;
+
+/**
+     * Reprezentuje databázi pojištěných
+     */
+    public class Databaze {
+
+        /**
+         * Název databáze
+         */
+        private String nazev;
+
+        /**
+         * Kolekce pojištených
+         */
+        private ArrayList<Pojisteny> pojisteni = new ArrayList<>();
+
+        /**
+         * Inicializuje novou instanci
+         *
+         * @param nazev
+         */
+        public Databaze(String nazev) {
+            this.nazev = nazev;
+        }
+
+        /**
+         *
+         * @return
+         */
+        @Override
+        public String toString() {
+            return "" + pojisteni;
+        }
+
+
+
+
+
 }
